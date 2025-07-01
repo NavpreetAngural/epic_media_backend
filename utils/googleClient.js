210d0bfe252cbf27ae4a8508c39608f6fc88968f
Navpreet <navangural175@gmail.com>
2025-07-01 23:38:55 +0530

    update code

--- utils/googleClient.js
+++ utils/googleClient.js
@@ -4,6 +4,6 @@ require("dotenv").config()
 exports.oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "postmessage"
+  "https://epic-media-backend.onrender.com"
 );
 
