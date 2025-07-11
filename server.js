6ca2f551193ffa6656512a2b0a59d1a9d1513171
Navpreet <navangural175@gmail.com>
2025-07-11 17:03:03 +0530

    backend code serverjs file fixed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 }))
 
 app.use(routes)
-app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 mongoose.connect(mongodb).then(() => {
     console.log("Database Connected Successfully");
